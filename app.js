c716b02218d0e4d533b884fab8e4bbd5bd00434c
Olajide Abdulrazzaq Folarin <folaraz11@gmail.com>
2022-05-07 13:48:28 +0100

    options issue fix

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 
 const PORT = process.env.PORT
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 //Authentication
 app.use("/api", authenticationRouter);
 app.use("/api", passwordRouter)
